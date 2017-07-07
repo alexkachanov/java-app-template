4623cfe5b2c95f8e5d069e568fa031e388cfb302
Alex Kachanov <alex@kachanov.com>
2017-07-07 22:28:59 +0900

    initial commit

--- src/main/java/com/kachanov/app/Main.java
+++ src/main/java/com/kachanov/app/Main.java
@@ -1,9 +1,16 @@
 package com.kachanov.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger( Main.class );
+
 	public static void main( String[] args ) {
-		//
+		logger.info( "app started" );
+		System.out.println( "app" );
+		logger.info( "app finished" );
 	}
-	
+
 }
