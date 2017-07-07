51dfec6fb6037993c1866c0e587756d064da4c13
Alex Kachanov <alex@kachanov.com>
2017-07-07 21:31:40 +0900

    initial commit

---
+++ src/main/java/com/kachanov/app/Main.java
@@ -0,0 +1,9 @@
+package com.kachanov.app;
+
+public class Main {
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ start.bat
@@ -0,0 +1,1 @@
+java 	-Dlog4j.configurationFile=libs/log4j2.properties 	-jar libs/app-2.0.jar
